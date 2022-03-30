eb360ee275b1e47da0811f156b7fc583a4b8053d
Vrush3099 <dhumalvrushali99@gmail.com>
2022-03-30 20:59:26 +0530

    to add javascript file

---
+++ Javascript/javascript.js.js
@@ -0,0 +1,27 @@
+//Addition of two number using function
+function Addition(num1,num2){
+   var addition=num1 + num2;
+   console.log("Addition of two number is:" +addition);
+}
+Addition(20,30)
+
+//Substraction of two number using function
+function Substraction(num1,num2){
+    var substraction=num1 - num2;
+    console.log("Substraction of two number is:" +substraction);
+ }
+Substraction(20,30)
+
+ //Multiplication of two number using function
+function Multiplication(num1,num2){
+    var multiplication=num1 * num2;
+    console.log("Multiplication of two number is:" +multiplication);
+ }
+ Multiplication(20,30)
+
+ //Division of two number using function
+function Division(num1,num2){
+    var division=num1 / num2;
+    console.log("Division of two number is:" +division);
+ }
+ Division(40,5)
